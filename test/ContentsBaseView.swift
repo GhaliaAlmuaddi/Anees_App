2b460ccff35781650cd1d39f80ee53019fc69830
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-10-12 11:04:13 +0300

    test

---
+++ test/ContentsBaseView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentsBaseView.swift
+//  test
+//
+//  Created by Ghalia Mohammed Al Muaddi on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentsBaseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentsBaseView()
+}
