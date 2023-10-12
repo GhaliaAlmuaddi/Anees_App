c99ef2a0f07488463b1ced18be10be6e4d36beab
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-10-12 11:42:12 +0300

    Ghalia Pages

---
+++ test/Recommendation.swift
@@ -0,0 +1,18 @@
+//
+//  Recommendation.swift
+//  test
+//
+//  Created by Ghalia Mohammed Al Muaddi on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Recommendation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Recommendation()
+}
