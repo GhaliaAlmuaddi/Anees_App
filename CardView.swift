fe643659db5a81f3b512cc85356c107841da6787
Hasna Ahmad <hassna.ahmad98@gmail.com>
2023-10-12 10:44:14 +0300

    Feelings View

---
+++ CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  test
+//
+//  Created by Hasna Ahmed on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
