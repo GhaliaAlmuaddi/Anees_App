783fe9289242cd8855b8bb596b7a396284ecac3b
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-10-12 11:43:11 +0300

    Ghalia Pages

--- test/ContentsBaseView.swift
+++ test/ContentsBaseView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentsBaseView: View {
+    var Content_title : String
+    var Content_description : String
+    var Content_image : String
+    var Content_link : String
+    var feeling_type : String
+    var Category : String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("Hello, World!")
-
-        
+        VStack{
+           /* NavigationView{
+                Text("")
+                    .toolbar{
+                        Button("تحديث"){}
+                        Text("الصوتيات")
+                            .multilineTextAlignment(.center)
+                    }} */
+            ZStack{
+                Rectangle().fill(Color("BlueOne")).frame(width: 500, height:500).position(x:150,y:130)
+                Image(Content_image).resizable().frame(width: 305.78 , height:293 ).cornerRadius(30).shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).offset(y:50).multilineTextAlignment(.center)
+            }.padding() //End of Zstack
+            VStack{
+                Text(Content_title).lineSpacing(1).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).padding()
+                Text(Content_description).multilineTextAlignment(.trailing).padding(20)
+            }.offset(x:10 , y:-30)
+            
+            Button(action: {
+             /*   Link(Destination: URL(string: "https://www.apple.com")!
+           )  */ }, label: {
+    
+                Text("للاستماع")
+                   
+               
+           })        }.ignoresSafeArea().padding() //End of big Vstack
     }
 }
 
+
+        
+  
+
 #Preview {
-    ContentsBaseView()
+    ContentsBaseView(Content_title : "" , Content_description : "" , Content_image : "" ,Content_link : "" , feeling_type: "" , Category : "" )
 }
--- test/Recommendation.swift
+++ test/Recommendation.swift
@@ -9,9 +9,34 @@ import SwiftUI
 
 struct Recommendation: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ContentsBaseView(Content_title: "بودكاست تنفس - كنبة السبت", Content_description: "نكبر فتكبر مهامنا وتكثر مسؤولياتنا، فيكبر قلقنا عشرات المرات، وبدلًا من أن يعمل القلق عمله الحقيقي في دفعنا لحياة أفضل، على العكس تمامًا، يشغلنا عن الحياة بأكملها..وننسى حتى كيف نتنفّس", Content_image: "كنبة السبت", Content_link: "ww.wwlink", feeling_type: "Worry" , Category : "Audio")
+         /* VStack{
+                /* NavigationView{
+                     Text("")
+                         .toolbar{
+                             Button("تحديث"){}
+                             Text("الصوتيات")
+                                 .multilineTextAlignment(.center)
+                         }} */
+                 ZStack{
+                     Rectangle().fill( .blueOne).frame(width: 500, height:500).position(x:150,y:130)
+                     Image("كنبة السبت").resizable().frame(width: 305.78 , height:293 ).cornerRadius(30).shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).position(x: 180, y: 255)
+                 }.padding() //End of Zstack
+                 VStack{
+                     Text("بودكاست تنفس - كنبة السبت").lineSpacing(1).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).padding()
+                     Text("نكبر فتكبر مهامنا وتكثر مسؤولياتنا، فيكبر قلقنا عشرات المرات، وبدلًا من أن يعمل القلق عمله الحقيقي في دفعنا لحياة أفضل، على العكس تمامًا، يشغلنا عن الحياة بأكملها..وننسى حتى كيف نتنفّس").multilineTextAlignment(.trailing).padding(20)
+                 }.offset(x:10 , y:-76)
+                 Button(action: {
+                  /*   Link(Destination: URL(string: "https://www.apple.com")!
+                )  */ }, label: {
+                    
+                     Text( "للاستماع").frame(width : 95, height :90 ,alignment: .center).background(.red).offset(y:-50).cornerRadius(8)
+                })
+             } //End of big Vstack
+          */
+          }
+ }
+
 
 #Preview {
     Recommendation()
